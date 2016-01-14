5662c8328d220db62d714a724aefbfb2de903cea
Richard Dawe <richdawe@gmail.com>
2016-01-14 23:12:02 +0000

    Add a basic NGINX proxy container in front of the app

--- docker.mk
+++ docker.mk
@@ -6,12 +6,21 @@ DOCKER = $(SUDO) docker
 default:	build
 
 .PHONY:	build
-build:	.build.stamp
+build:	.build.stamp .build.proxy.stamp
 
 .build.stamp:	Dockerfile
 	$(DOCKER) build -t requestbin-app .
 	touch $@
 
+.build.proxy.stamp:	nginx/Dockerfile nginx/requestbin.conf
+	$(DOCKER) build -t requestbin-proxy nginx
+	touch $@
+
+# Use Redis as the database backend for RequestBin.
+# Proxy requests through NGINX, so we can add some authentication
+# for creating and viewing bins.
+#
+# NGINX images: https://github.com/nginxinc/docker-nginx
 # Redis images: https://registry.hub.docker.com/u/library/redis/
 .PHONY:	run
 run:	build
@@ -19,18 +28,24 @@ run:	build
 		-d redis:2.8 \
 		redis-server --appendonly yes
 	$(DOCKER) run --name requestbin-app \
-		-d -p 8000:8000 --link requestbin-redis:redis \
-		-e REQUESTBIN_REDIS_HOST=redis \
+		-d --link requestbin-redis:db \
+		-e REQUESTBIN_REDIS_HOST=db \
 		-e REQUESTBIN_STORAGE=requestbin.storage.redis.RedisStorage \
 		requestbin-app
+	$(DOCKER) run --name requestbin-proxy \
+		-d -p 8000:80 --link requestbin-app:app \
+		requestbin-proxy
 
 .PHONY:	stop
 stop:
+	$(DOCKER) stop requestbin-proxy
 	$(DOCKER) stop requestbin-app
 	$(DOCKER) stop requestbin-redis
+	$(DOCKER) rm requestbin-proxy
 	$(DOCKER) rm requestbin-app
 	$(DOCKER) rm requestbin-redis
 
 .PHONY:	clean
 clean:
 	rm -f .build.stamp
+	rm -f .build.proxy.stamp
