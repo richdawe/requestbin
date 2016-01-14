44e14d21c6381504e4f05e442f49b3795b0eaa31
Richard Dawe <richdawe@gmail.com>
2016-01-14 17:57:52 +0000

    Link requestbin in Docker container to a Redis container

--- docker.mk
+++ docker.mk
@@ -6,15 +6,31 @@ DOCKER = $(SUDO) docker
 default:	build
 
 .PHONY:	build
-build:
+build:	.build.stamp
+
+.build.stamp:	Dockerfile
 	$(DOCKER) build -t requestbin-app .
+	touch $@
 
+# Redis images: https://registry.hub.docker.com/u/library/redis/
 .PHONY:	run
-run:
-	$(DOCKER) run -d -p 8000:8000 --name=requestbin-app requestbin-app
+run:	build
+	$(DOCKER) run --name requestbin-redis \
+		-d redis:2.8 \
+		redis-server --appendonly yes
+	$(DOCKER) run --name requestbin-app \
+		-d -p 8000:8000 --link requestbin-redis:redis \
+		-e REQUESTBIN_REDIS_HOST=redis \
+		-e REQUESTBIN_STORAGE=requestbin.storage.redis.RedisStorage \
+		requestbin-app
 
-.PHONY:
+.PHONY:	stop
 stop:
 	$(DOCKER) stop requestbin-app
+	$(DOCKER) stop requestbin-redis
 	$(DOCKER) rm requestbin-app
+	$(DOCKER) rm requestbin-redis
 
+.PHONY:	clean
+clean:
+	rm -f .build.stamp
